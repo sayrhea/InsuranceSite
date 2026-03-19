e1e6cbd1ed223be43b33387a79856db13842b670
Priyanka Mukherjee <rhea.uco@gmail.com>
2026-03-20 00:35:19 +0530

    Services page completed

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,6 +1,13 @@
 import { services } from "../../constants/service";
+import { useState } from "react";
 
 const Services = () => {
+    const [activeService, setActiveService] = useState(1);
+
+    const handleServiceClick = (id) => {
+        setActiveService(id);
+    }
+
     return (
         <main id="services">
             <div className="services-header mx-20 pb-20">
@@ -16,11 +23,17 @@ const Services = () => {
             <hr className="white-line" />
             <div className="services-body">
                 <ul className="">{services.map((service) => (
-                    <li key={service.id} className="service-list-item">
-                        <div className="flex ml-20 mr-60">
-                            <h1>{service.id}</h1>
-                            <h2>{service.title}</h2>
-                            <p>{service.desc}</p>
+                    <li key={service.id} className={`${activeService === service.id ? "bg-blue-100 text-black" : ""}`}>
+                        <div
+                            className={
+                                `lg:h-[300px] flex lg:flex-row flex-col md:justify-center justify-around 
+                                md:items-center lg:gap-40 gap-10 md:ml-20 ml-5 md:mr-60 mr-15 
+                                `}
+                            onMouseEnter={() => handleServiceClick(service.id)}
+                            onMouseLeave={() => handleServiceClick(1)}>
+                            <h1 className="lg:text-7xl text-4xl font-bold">{service.id}</h1>
+                            <h2 className="text-3xl w-[300px]">{service.title}</h2>
+                            <p className="leading-[35px]">{service.desc}</p>
                         </div>
                         <hr className="white-line" />
                     </li>
